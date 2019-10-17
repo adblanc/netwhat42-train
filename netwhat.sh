357c8846eb19794a7b424edaecec8e1821726aef
ifreez <ifreezbackagain@gmail.com>
2019-10-17 12:23:38 +0200

    17 questions

---
+++ netwhat.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+path_q="srcs/questions.txt"
+path_answ="srcs/answers/"
+questions_nbr=17
+let "step = 1"
+
+bold=`tput bold`
+underline=`tput smul`
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+answers=('2' '6' '1' '7' '6' '1' '4' '3' '5' '2' '0' '1' '2' '7' '0' '3' '2')
+
+function select_option {
+
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "   $1 "; }
+    print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    key_input()        { read -s -n3 key 2>/dev/null >&2
+                         if [[ $key = $ESC[A ]]; then echo up;    fi
+                         if [[ $key = $ESC[B ]]; then echo down;  fi
+                         if [[ $key = ""     ]]; then echo enter; fi; }
+
+    # initially print empty new lines (scroll down if at bottom of screen)
+    for opt; do printf "\n"; done
+
+    # determine current screen position for overwriting the options
+    local lastrow=`get_cursor_row`
+    local startrow=$(($lastrow - $#))
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local selected=0
+    while true; do
+        # print options by overwriting the last lines
+        local idx=0
+        for opt; do
+            cursor_to $(($startrow + $idx))
+            if [ $idx -eq $selected ]; then
+                print_selected "$opt"
+            else
+                print_option "$opt"
+            fi
+            ((idx++))
+        done
+
+        # user key control
+        case `key_input` in
+            enter) break;;
+            up)    ((selected--));
+                   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+            down)  ((selected++));
+                   if [ $selected -ge $# ]; then selected=0; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    return $selected
+}
+
+function select_opt {
+    select_option "$@" 1>&2
+    local result=$?
+    echo $result
+    return $result
+}
+
+while [ $step -le $questions_nbr ]
+	do
+		responses=()
+		let "j = 0"
+		echo -e "${bold}${underline}Question $step\n"
+		tail -n+$step $path_q | head -n1
+		echo -e "\n${reset}"
+		while IFS= read -r line
+			do
+  				responses[$j]=$line
+				let "j = j + 1"
+			done < "$path_answ$step.txt"
+		case `select_opt "${responses[@]}"` in
+			${answers["$step - 1"]}) echo -e "${green}Bonne reponse !\n\n${reset}";;
+    			*) echo -e "${red}Mauvaise reponse !\n\n${reset}";;
+		esac
+		unset responses
+		let "step = step + 1"
+	done
